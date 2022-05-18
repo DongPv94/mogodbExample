991ed980442f81fc8b93cc246ad9e25cf569d2fa
dong.pv <phamvan.dong@etc.vn>
2022-05-18 09:49:12 +0700

    add more table in mogodb

---
+++ server.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const { PORT, mongoUri } = require("./config");
+const cors = require("cors");
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+const bucketListItemRoutes = require("./routes/api/bucketListItems");
+const userLoginRoutes = require("./routes/api/userLoginApi");
+const path = require("path");
+
+app.use(cors());
+app.use(morgan("tiny"));
+app.use(bodyParser.json());
+
+mongoose
+  .connect(mongoUri, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+  })
+  .then(() => console.log("MongoDB database Connected..."))
+  .catch((err) => console.log(err));
+
+
+app.use("/api/bucketListItems", bucketListItemRoutes);
+app.use("/api/userLogin", userLoginRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/dist"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "dist", "index.html"));
+  });
+}
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT}`)
+);
